a5dd34f1d48aedd37e833e815b5f65ffa62c37cc
azu <azuciao@gmail.com>
2017-05-07 00:53:57 +0900

    Initial Commit

---
+++ 01-State/01-ToJSONable-State.ts
@@ -0,0 +1,202 @@
+import { Payload } from "almin";
+import * as assert from "assert";
+// This is concept model of State
+// Simple principle:
+interface StatePrinciple<T> {
+    constructor(state: T): any;
+    toJSON(): T;
+}
+// Implementation
+// It is based `JSON.stringify` default algorithm
+// https://tc39.github.io/ecma262/#sec-serializejsonobject
+const ToJSONObject = <T>(object: State<T>): T => {
+    const json: any = {};
+    const keys = Object.keys(object);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        json[key] = (object as any)[key];
+    }
+    return json as T;
+};
+
+abstract class State<T> {
+    constructor(state: T) {
+    }
+
+    merge(state: Partial<T>): State<T> {
+        return new (this as any).constructor({
+            ...this as State<T>,
+            ...state as any
+        });
+    }
+
+    toJSON(): T {
+        return ToJSONObject(this);
+    }
+}
+
+// Implement State sub class
+// domain
+class MyDomain {
+    value: string;
+
+    constructor(value: string) {
+        this.value = value;
+    }
+}
+
+interface MyStateProps {
+    value: string;
+    isLoading: boolean;
+}
+
+class MyState extends State<MyStateProps> implements MyStateProps {
+    value: string;
+    isLoading: boolean;
+
+    constructor(props: MyStateProps) {
+        super(props);
+        this.value = props.value;
+        this.isLoading = props.isLoading;
+    }
+
+    merge(state: Partial<MyState>): MyState {
+        return new (this as any).constructor({
+            ...this as MyState,
+            ...state
+        });
+    }
+
+    from(domain: MyDomain): MyState {
+        return this.merge({
+            value: domain.value
+        });
+    }
+
+    reduce(payload: Payload): MyState {
+        switch (payload.type) {
+            case "start":
+                return this.merge({
+                    isLoading: true
+                });
+            case "stop":
+                return this.merge({
+                    isLoading: false
+                });
+            default:
+                return this;
+        }
+    }
+}
+
+
+class ExtendState extends State<MyStateProps> implements MyStateProps {
+    value: string;
+    isLoading: boolean;
+
+    extensionValue: string;
+
+    constructor(props: MyStateProps) {
+        super(props);
+        this.value = props.value;
+        this.isLoading = props.isLoading;
+        this.extensionValue = "extension";
+    }
+
+    get extensionGetter() {
+        return this.extensionValue + "ext";
+    }
+
+    merge(state: Partial<MyStateProps>): ExtendState {
+        return new ExtendState({
+            ...this as ExtendState,
+            ...state
+        });
+    }
+
+    from(domain: MyDomain): ExtendState {
+        return this.merge({
+            value: domain.value
+        });
+    }
+
+    reduce(payload: Payload): ExtendState {
+        switch (payload.type) {
+            case "start":
+                return this.merge({
+                    isLoading: true
+                });
+            case "stop":
+                return this.merge({
+                    isLoading: false
+                });
+            default:
+                return this;
+        }
+    }
+
+    toJSON() {
+        return {
+            value: this.value,
+            isLoading: this.isLoading,
+        }; // without extensionValue
+    }
+}
+
+
+// Pass MyState
+testYourStore(MyState);
+// Fail BadState
+testYourStore(ExtendState);
+
+function testYourStore(StoreConstructor: new (..._: Array<any>) => MyState | ExtendState) {
+    describe(`${(StoreConstructor as any).name}`, () => {
+        it("Store<T> can unwrap T by Store#toJSON()", () => {
+            const stateProps = { value: "value", isLoading: false };
+            const aState = new StoreConstructor(stateProps);
+            assert.deepStrictEqual(aState.toJSON(), stateProps);
+        });
+
+        it("new Store === new Store", () => {
+            const stateProps = { value: "value", isLoading: false };
+            const aState = new StoreConstructor(stateProps);
+            const bState = new StoreConstructor(stateProps);
+            assert.deepStrictEqual(aState.toJSON(), bState.toJSON());
+        });
+
+        it("new Store === new Store(new Store())", () => {
+            const stateProps = { value: "value", isLoading: false };
+            const aState = new StoreConstructor(stateProps);
+            const bState = new StoreConstructor(new MyState(stateProps));
+            assert.deepStrictEqual(aState.toJSON(), bState.toJSON());
+        });
+
+        it("create Store from Payload", () => {
+            const initialState = new StoreConstructor({
+                value: "",
+                isLoading: false
+            });
+            const state = initialState.reduce({
+                type: "start"
+            });
+            assert.deepStrictEqual(state.toJSON(), {
+                value: "",
+                isLoading: true
+            });
+        });
+
+        it("create Store from Domain", () => {
+            const myDomain = new MyDomain("domain value");
+            const initialState = new StoreConstructor({
+                value: "",
+                isLoading: false
+            });
+            const state = initialState.from(myDomain);
+            assert.deepStrictEqual(state.toJSON(), {
+                value: "domain value",
+                isLoading: false
+            });
+        });
+
+    });
+}
---
+++ README.md
@@ -0,0 +1,34 @@
+# almin-thinking
+
+:thought_balloon: Documents/Concept/Experimental Design for Almin.
+
+## Structures
+
+### State
+
+- [01-State/01-ToJSONable-State.ts](./01-State/01-ToJSONable-State.ts)
+
+## Inspired
+
+- [reactjs/react-future: Specs & docs for potential future and experimental React APIs and JavaScript syntax.](https://github.com/reactjs/react-future "reactjs/react-future: Specs &amp; docs for potential future and experimental React APIs and JavaScript syntax.")
+
+## Contributing
+
+Pull requests and stars are always welcome.
+
+For bugs and feature requests, [please create an issue](https://github.com/almin/almin-thinking/issues).
+
+1. Fork it!
+2. Create your feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. Submit a pull request :D
+
+## Author
+
+- [github/azu](https://github.com/azu)
+- [twitter/azu_re](https://twitter.com/azu_re)
+
+## License
+
+MIT © azu
