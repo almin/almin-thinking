e74e77a2bf915901bc445bb4f8b2926b248c8dc4
azu <azuciao@gmail.com>
2018-03-03 02:11:29 +0900

    Update

--- 02-UseCase/04-Conditional-UseCase.ts
+++ 02-UseCase/04-Conditional-UseCase.ts
@@ -1,7 +1,19 @@
 import { UseCase, Context, Dispatcher, Store } from "almin";
-type A1<T> = T extends (a1: infer R1) => any ? [R1] : any
-type A2<T> = T extends (a1: infer R1, a2: infer R2) => any ? [R1, R2] : any
-type A3<T> = T extends (a1: infer R1, a2: infer R2, x: infer R3) => any ? [R1, R2, R3] : any
+type A<T> = T extends (a1: infer R1) => any
+    ? [R1]
+    : T extends (a1: infer R1, a2: infer R2) => any
+    ? [R1, R2]
+    : T extends (a1: infer R1, a2: infer R2, a3: infer R3) => any
+    ? [R1, R2, R3]
+    : never
+type A1<T> = T extends (a1: infer R1) => any
+    ? R1 : never
+type A2<T> = T extends (a1: any, a2: infer R2) => any
+    ? R2 : never
+type A3<T> = T extends (a1: any, a2: any, a3: infer R3) => any
+    ? R3 : never
+// type A2<T> = T extends (a1: infer R1, a2: infer R2) => any ? [R1, R2] : never
+// type A3<T> = T extends (a1: infer R1, a2?: infer R2, a3?: infer R3) => any ? [R1, R2, R3] : never
 class UseCaseExecutor<T extends UseCase> {
     useCase: T;
 
@@ -9,9 +21,9 @@ class UseCaseExecutor<T extends UseCase> {
         this.useCase = useCase;
     }
 
-    execute(a1: A1<T["execute"]>[0]): Promise<void>;
-    execute(a1: A2<T["execute"]>[0], a2: A2<T["execute"]>[1]): Promise<void>;
-    execute(a1: A3<T["execute"]>[0], a2: A3<T["execute"]>[1], a3: A3<T["execute"]>[2]): Promise<void>;
+    execute<P extends A<T["execute"]>>(a1: P[0]): Promise<void>;
+    execute<P extends A<T["execute"]>>(a1: P[0], a2: P[1]): Promise<void>;
+    execute<P extends A<T["execute"]>>(a1: P[0], a2: P[1], a3: P[2]): Promise<void>;
     execute(...args: any[]): Promise<void> {
         return this.useCase.execute(...args);
     }
@@ -23,18 +35,16 @@ class MyUseCase extends UseCase {
         return Promise.resolve(value);
     }
 }
-
 class MyUseCase2 extends UseCase {
     execute(x1: number, x2: string) {
     }
 }
-
 class MyUseCase3 extends UseCase {
     execute(x1: number, x2: string, x3: { key: string }) {
     }
 }
 
-type r = A3<MyUseCase3["execute"]>
+type r = A<MyUseCase3["execute"]>
 
 new UseCaseExecutor(new MyUseCase())
     .execute(1)
@@ -47,9 +57,9 @@ new UseCaseExecutor(new MyUseCase2())
     .then(value => {
         console.log(value);
     });
-// Should be error
+// Should be error - missing arguments
 new UseCaseExecutor(new MyUseCase3())
-    .execute(1, 1, { key: "" })
+    .execute(1, "")
     .then(value => {
         console.log(value);
     });
